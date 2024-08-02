79464ea875b2ddec7144bedb3e0b9880fdbfe057
Ankita001812 <132539778+Ankita001812@users.noreply.github.com>
2024-08-03 00:31:06 +1000

    initial

---
+++ frens/src/pages/Login.js
@@ -0,0 +1,62 @@
+import React from "react";
+import bg from "../images/fbg.jpg";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div className="container-bg">
+      <div className="container d-flex justify-content-center align-items-center vh-100">
+        
+        <div className="row w-100 ">
+          <div className="col-6  align-items-start text-white position-relative" style={{marginright:"0px"}}>
+            <img src={bg} alt="background" className="bg-img-left" />
+            <div className="content">
+              <h1 className="fw-bold">Hello world!</h1>
+              <p className="lead">
+                Want to know what's going on in your friends' lives? Stay connected with your friends.
+              </p>
+              <span>Don't have an account?  </span>
+              
+              <button type="submit" className="btn btn-outline-info">
+                  Register
+                </button>
+            </div>
+          </div>
+          <div className="col-6 d-flex justify-content-center align-items-center" style={{paddingLeft:"0px"}}>
+            <form className="w-75 bg-white p-4 rounded">
+              <div className="mb-3 text-start">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  id="email"
+                  name="email"
+                />
+              </div>
+              <div className="mb-3 text-start">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  id="password"
+                  name="pass"
+                />
+              </div>
+              <div className="d-grid">
+                <button type="submit" className="btn btn-success">
+                  Log In
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
