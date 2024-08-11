0dca205aab981bbe4876fa2feca8b2e54e221b8f
Ankita001812 <132539778+Ankita001812@users.noreply.github.com>
2024-08-12 03:27:58 +1000

    initsial nav

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Frens
 A social media
+
+
+# command to intall the needed libarary for using the icons from material icons
+## npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
--- frens/src/components/Nav.js
+++ frens/src/components/Nav.js
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from "react";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import GridViewRoundedIcon from "@mui/icons-material/GridViewRounded";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import { Link } from "react-router-dom";
+import "./Nav.css";
 
 const Nav = () => {
   return (
-    <div>
-      <div className="diplay-3">Nav</div>
-    </div>
-  )
-}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid fw-bold">
+        <Link to="/" className="navbar-brand d-flex align-items-center">
+          <span className="display-4 fw-bold"><i>FrEnS</i></span>
+          
+        </Link>
 
-export default Nav
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav  mb-2 mb-lg-0">
+            <li className="nav-item">
+              <HomeRoundedIcon className="icon" />
+            </li>
+            <li className="nav-item">
+              <GridViewRoundedIcon className="nav-icon" />
+            </li>
+          </ul>
+          <form className="d-flex search-form">
+            <SearchRoundedIcon className="icon search-icon" />
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
